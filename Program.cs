bd41892df9779731f9c2055c40f927cc1b322877
Veronika <vvnika@mail.ru>
2022-07-21 02:45:46 +1100

    add "Семинар_5_Задача 38"

--- Program.cs
+++ Program.cs
@@ -48,6 +48,7 @@ void OutputArrayString(int[] arrayString)
 */
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
+/*
 Console.WriteLine("Задача 36");
 
 int[] numbers = new int[4];
@@ -92,4 +93,57 @@ void SumStandingElementsOddPositions(int[] number)
     }
     Console.WriteLine($" -> сумма чисел стоящих на нечётных позициях " +
                       $"в массиве равна: {sum}");
+}
+*/
+//Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+Console.WriteLine("Задача 36");
+double[] numbers = new double [10];
+int n = numbers.Length;
+
+FillArray(numbers);
+OutputArrayString(numbers);
+DifferenceМinМax(numbers);
+
+void FillArray(double[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble() * 100);
+    }
+}
+
+void DifferenceМinМax(double[] number)
+{
+    double min = number[0];
+    double max = number[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (number[i] > max)
+        {
+            max = number[i];
+        }
+        if (number[i] < min)
+        {
+            min = number[i];
+        }
+    }
+    double diff = max - min;
+    Console.WriteLine($" -> разница между максимальным элементом {max} " +
+                      $"и минимальным элементом {min} массива равна: {diff}");
+}
+
+void OutputArrayString(double[] arrayString)
+{
+    string stringArray = "[";
+
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        stringArray += $"{arrayString[i]}, ";
+    }
+    int n = stringArray.Length;
+    stringArray = stringArray.Remove(n - 2, 2);
+    stringArray += "]";
+    Console.Write($"Массив: {stringArray}");
 }
